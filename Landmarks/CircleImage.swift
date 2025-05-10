12e3f7fff50255cd76d8819c1a903ae726c511be
Gabriel Olivan <gabrielolivan@gmail.com>
2025-05-10 20:02:34 -0300

    SwiftUI essentials Creating and combining views Status: Completed (1/3 chapter 1)

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Gabriel Olivan on 10/05/25.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(Color.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,21 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-                .fontWeight(.bold)
+        VStack {
+            MapView()
+                .frame(height: 300)
             
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .fontWeight(.bold)
                 
-                Spacer()
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
                 
-                Text("California")
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
             
         
     }
